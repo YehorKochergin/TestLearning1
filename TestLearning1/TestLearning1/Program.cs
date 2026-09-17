7ebf4f57447325ec12e71b62a4dea8c6f76864a7
YehorKochergin <egorkocergin033@gmail.com>
2026-09-17 11:57:12 +0300

    Сalculation of a Fibonacci number using a recurrence formula

--- TestLearning1/TestLearning1/Program.cs
+++ TestLearning1/TestLearning1/Program.cs
@@ -1,6 +1,12 @@
-﻿string firstName = "Bob";
-Console.WriteLine(firstName);
-Console.WriteLine(firstName);
-Console.WriteLine(firstName);
-Console.WriteLine(firstName);
-Console.WriteLine(firstName);
+﻿Console.WriteLine("enter the value of n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+Console.WriteLine(fibonacci(n));
